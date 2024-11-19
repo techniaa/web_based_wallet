4c212ddabc0937834a013bbeda5301df2a741125
techniaa <nehagc8485@gmail.com>
2024-11-19 20:17:00 +0530

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import './NavBar.css'
+const NavBar = () => {
+    return (
+        <nav>
+            <span>ETH-WALLET</span>
+            <a href="" target="_blank"><button>Github</button></a>
+        </nav>
+    )
+}
+
+export default NavBar
